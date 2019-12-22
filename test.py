974d43dd770b592e62f1cbab2fb1888b99f41fb5
Stephen Xiao <stephen2015@126.com>
2019-12-22 19:22:46 +0800

    create test script to verify the action between SourceTree and Github

---
+++ test.py
@@ -0,0 +1,11 @@
+# Coding: utf-8
+# Author: Stephen Xiao
+# Date  :20191222
+# Function: It is used for testing
+
+import os
+import sys
+import time
+import date
+
+print("Hello world, Let's start to work!")
