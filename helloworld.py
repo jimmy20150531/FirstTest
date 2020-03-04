70cae72afef8106fcd0429db6ba8a7f28330bf5c
stephen2015 <stephen2015@126.com>
2020-03-04 20:37:42 +0800

    change the print and date information

--- helloworld.py
+++ helloworld.py
@@ -1,9 +1,7 @@
 # Coding: utf-8
 # Author: Stephen Xiao
-# Date  :20191222
+# Date  :20200304
 # Function: It is used for testing
-
-print("Let's start to work!")
 
 define sayHello(void):
 	print("Hello world!")
