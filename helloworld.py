78c7bf74fd319d28bb6f7370b583de348794300c
stephen2015 <stephen2015@126.com>
2020-03-04 20:24:04 +0800

    say hello world.

--- helloworld.py
+++ helloworld.py
@@ -3,17 +3,7 @@
 # Date  :20191222
 # Function: It is used for testing
 
-import os
-import sys
-import time
-import date
-
-from helloworld import sayHello
-
 print("Let's start to work!")
 
-sayHello()
-
-time.sleep(10)
-
-
+define sayHello(void):
+	print("Hello world!")
